d8e2e884c7671240ec0697450d1d3c61ce105ce5
DEGRYSE ALEXIS <alexis.degryse@gmail.com>
2015-02-16 17:20:31 +0100

    add who is who

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
     $('input[type=radio]').change(function() { // When a slot is checked
         nbSlotChecked = $('input[type=radio]:checked').length; // let's recalculte the number of slots checked...
         if(nbSlotChecked%2 === 0) { // ... to define the current turn
-            $(this).addClass('player-1');
-        } else {
             $(this).addClass('player-2');
+        } else {
+            $(this).addClass('player-1');
         }
     }); 
 
